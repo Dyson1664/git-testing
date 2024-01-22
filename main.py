cc13b6ce5edf45f5548db039b3cf54aa377558bd
david reilly <davidreilly02@gmail.com>
2024-01-22 15:36:24 +0700

    added test.py and git ignore

---
+++ main.py
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3, 4, 5]
+letters = ['a', 'b', 'c', 'd', 'e']
+
+list1 = []
+for number, letter in zip(numbers, letters):
+    list1.append((number, letter))
+
+print(list1)
+
+
+for i in enumerate(letters, 1):
+    print(i)
+
+
+names = ['Alice', 'Bob', 'Charlie']
+ages = [24, 50, 18]
+
+for i, (name, age) in enumerate(zip(names, ages), 1):
+    print(i, name, age)
+
